1d06e09c6a1957197adfc01b931dd67af2fca999
fahad1997 <fahadh1997@gmail.com>
2017-09-25 12:20:50 +0600

    Add files via upload

---
+++ 5.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main()
+{
+    int m,n,i,j,k=0,a[100],b[100],c[200];
+    //enter the elements of A
+    printf("Enter how many elements you want for A:");
+    scanf("%d",&n);
+    printf("Enter the elements:");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+
+    //Enter the values of B
+    printf("Enter hew many elements you want for B:");
+    scanf("%d",&m);
+    prinf("Enter the elements:");
+    for(j=0;j<m;j++)
+    {
+       scanf("%d",&b[j]);
+    }
+
+    for(i=0;i<n;i++)
+    {
+        c[k]=a[i];
+        k++;
+    }
+    for(j=0;j<m;j++)
+    {
+        c[k]=b[j];
+        k++;
+    }
+
+    printf("The total elements are:\t");
+    for(k=0;k<m+n;k++)
+    {
+        printf("%d\t",c[k]);
+    }
+    return 0;
+}
