97aa0e0837107c6f14bd9a19903bd420d44a9549
Shah Fahad Hossain <fahadh1997@gmail.com>
2017-10-25 14:18:54 +0600

    Add files via upload

---
+++ Deletion.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+int main()
+{
+    int a[10],i,k=-1,n,m;
+    printf("How many elements you want:");
+    scanf("%d",&n);
+    printf("Enter elements:");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+
+    printf("which number you want to delete:");
+    scanf("%d",&m);
+
+    for(i=0;i<n-1;i++)
+    {
+        if(a[i]==m)
+        {
+            k=i;
+            break;
+        }
+    }
+
+    if(k==-1)
+    {
+        printf("The data is not found...\n");
+
+        for(i=0;i<n;i++)
+         {
+           printf("%d\t",a[i]);
+         }
+    }
+    else
+    {
+        for(i=k;i<n;i++)
+          {
+            a[i]=a[i+1];
+          }
+
+        for(i=0;i<n-1;i++)
+          {
+            printf("%d\t",a[i]);
+          }
+    }
+
+    return 0;
+}
